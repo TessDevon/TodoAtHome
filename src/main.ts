39756a0af0ccece6b40fdc33f4ccab209af9f8d2
Therese <therese.holen22@gmail.com>
2022-12-12 10:58:45 +0100

    Hidden code from the original files. Built an HTML document to start from, part 1. Header, main with sections sorting and Todo fields for finished articles.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import './style/style.scss';
 
+
+
+
+
+
+/*
 // All kod härifrån och ner är bara ett exempel för att komma igång
 
 // I denna utils-fil har vi lagrat funktioner som ofta används, t.ex. en "blanda array"-funktion
@@ -16,7 +22,8 @@ const myShuffledCardDeck = shuffle(exampleCardDeck);
  * @param this - Det HTML-element som har klickats på
  * @return {void}
  */
-function flipCard(this: HTMLElement): void {
+
+/*function flipCard(this: HTMLElement): void {
   if (this !== undefined) {
     this.classList.toggle('visible');
   }
@@ -37,3 +44,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = cardString;
 document.querySelectorAll('.card').forEach((card) => {
   card.addEventListener('click', flipCard);
 });
+*/
