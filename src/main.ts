45ad329ae699c9be8388f4d3f8aadde03415d0f3
Therese <therese.holen22@gmail.com>
2022-12-14 11:31:09 +0100

    Added function for categorycolor.

--- src/main.ts
+++ src/main.ts
@@ -6,48 +6,66 @@ const todoArticles = [
   {
     toDaysDate: '2022-12-12',
     deadLineDate: '2023-03-15, 05:30',
-    category: 'categoryGarden',
+    category: 'Trädgård',
     toDoName: 'Första trädgårdstunnan',
     description: 'FÖrsta tömningen av trädgårdstunnan. Fyll på under veckan!',
   }, {
     toDaysDate: '2022-12-12',
     deadLineDate: '2023-05-30, 15:30',
-    category: 'categoryHandyman',
+    category: 'Hantverkare',
     toDoName: 'Sotaren påbesök',
     description: 'Fråga om olika förbränningsalternativ.',
   }, {
     toDaysDate: '2022-12-14',
     deadLineDate: '2023-06-30, 08:00',
-    category: 'categortHouse',
+    category: 'Hus och hem',
     toDoName: 'Måla om garaget',
     description: 'Målning av garage denna veckan med förtvätt och målning.',
   }, {
     toDaysDate: '2022-12-14',
     deadLineDate: '2023-03-05, 09:00',
-    category: 'categoryGarden',
+    category: 'Trädgård',
     toDoName: 'Första sådden',
     description: 'Börja förså Physalis och Vattenmelon.',
   }, {
     toDaysDate: '2022-12-14',
     deadLineDate: '2023-04-15, 10:00',
-    category: 'categoryGarden',
+    category: 'Trädgård',
     toDoName: 'Andra sådden',
     description: 'Dags att förså tomat och gurka',
   }, {
     toDaysDate: '2022-12-13',
     deadLineDate: '2023-03-15, 5:30',
-    category: 'categoryGarden',
+    category: 'Trädgård',
     toDoName: 'Första trädgårdstunnan',
     description: 'FÖrsta tömningen av trädgårdstunnan. Fyll på under veckan!',
   }];
 
 /**
- * Sortering
- * Loop där objkekten körs i en array
- * Lägga in HTML strukturen.
- *
- *
+ * Sortering, startdatum, deadline, namn (i bokstavsordning) och visas i kategori (i bokstavsordning)?
+ * Loop där objkekten körs i en array Klar
+ * Lägga in HTML strukturen KLAR
+ * Lägga till bakgrundsfärg till kategorierna?
+ * 
  */
+
+function addColorToCategorys() {
+  const categoryParts = document.querySelectorAll('#category');
+  console.log(categoryParts[0].innerHTML);
+
+  for (let i = 0; i < categoryParts.length; i++) {
+    if (categoryParts[i].innerHTML === 'Trädgård') {
+      categoryParts[i].style.backgroundColor = 'green';
+      categoryParts[i].style.color = 'white';
+    } else if (categoryParts[i].innerHTML === 'Hus och hem') {
+      categoryParts[i].style.backgroundColor = 'darkblue';
+      categoryParts[i].style.color = 'white';
+    } else if (categoryParts[i].innerHTML === 'Hantverkare') {
+      categoryParts[i].style.backgroundColor = 'purple';
+      categoryParts[i].style.color = 'white';
+    }
+  }
+}
 
 function updateTodoList() {
   toDoItemSection.innerHTML = '';
@@ -59,6 +77,7 @@ function updateTodoList() {
       <span class="toDaysDateClass" id="toDaysDate">${todoArticles[i].toDaysDate}</span>
       <span class="deadLineDateClass" id="deadLineDate">${todoArticles[i].deadLineDate}</span>
     </div>
+    <p class="categoryClass" id="category">${todoArticles[i].category}</p>
     <h2 class="toDoNameClass" id="toDoName">${todoArticles[i].toDoName}</h2>
     <p class="descriptionClass" id="description">${todoArticles[i].description}</p>
     <p class="fiveDaysToDeadlineP" id="fiveDaysToDeadline"></p>
@@ -68,6 +87,8 @@ function updateTodoList() {
     </div>      
   </article>`;
   }
+
+  addColorToCategorys();
 }
 
 updateTodoList();
