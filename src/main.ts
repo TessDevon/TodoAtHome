2e5a3bf491ebbeb46bdd1ef102423e29c80b63a1
Therese <therese.holen22@gmail.com>
2022-12-14 09:12:17 +0100

    Coded HTML structure for todo items in JS/TS.  Added a function with a loop that write out my array of todos object.

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,73 @@
 import './style/style.scss';
 
-/*
-// All kod härifrån och ner är bara ett exempel för att komma igång
+const toDoItemSection = document.querySelector('#toDoItemSection');
 
-// I denna utils-fil har vi lagrat funktioner som ofta används, t.ex. en "blanda array"-funktion
-import { shuffle } from './utils';
-
-// I denna fil har vi lagrat vår "data", i detta exempel en ofullständig kortlek
-import exampleCardDeck from './exampleArray';
-
-// Blanda kortleken
-const myShuffledCardDeck = shuffle(exampleCardDeck);
+const todoArticles = [
+  {
+    toDaysDate: '2022-12-12',
+    deadLineDate: '2023-03-15, 05:30',
+    category: 'categoryGarden',
+    toDoName: 'Första trädgårdstunnan',
+    description: 'FÖrsta tömningen av trädgårdstunnan. Fyll på under veckan!',
+  }, {
+    toDaysDate: '2022-12-12',
+    deadLineDate: '2023-05-30, 15:30',
+    category: 'categoryHandyman',
+    toDoName: 'Sotaren påbesök',
+    description: 'Fråga om olika förbränningsalternativ.',
+  }, {
+    toDaysDate: '2022-12-14',
+    deadLineDate: '2023-06-30, 08:00',
+    category: 'categortHouse',
+    toDoName: 'Måla om garaget',
+    description: 'Målning av garage denna veckan med förtvätt och målning.',
+  }, {
+    toDaysDate: '2022-12-14',
+    deadLineDate: '2023-03-05, 09:00',
+    category: 'categoryGarden',
+    toDoName: 'Första sådden',
+    description: 'Börja förså Physalis och Vattenmelon.',
+  }, {
+    toDaysDate: '2022-12-14',
+    deadLineDate: '2023-04-15, 10:00',
+    category: 'categoryGarden',
+    toDoName: 'Andra sådden',
+    description: 'Dags att förså tomat och gurka',
+  }, {
+    toDaysDate: '2022-12-13',
+    deadLineDate: '2023-03-15, 5:30',
+    category: 'categoryGarden',
+    toDoName: 'Första trädgårdstunnan',
+    description: 'FÖrsta tömningen av trädgårdstunnan. Fyll på under veckan!',
+  }];
 
 /**
- * Vänder upp/ner på det klickade kortet genom att toggla en CSS-klass.
- * @param this - Det HTML-element som har klickats på
- * @return {void}
+ * Sortering
+ * Loop där objkekten körs i en array
+ * Lägga in HTML strukturen.
+ *
+ *
  */
 
-/*function flipCard(this: HTMLElement): void {
-  if (this !== undefined) {
-    this.classList.toggle('visible');
+function updateTodoList() {
+  toDoItemSection.innerHTML = '';
+
+  for (let i = 0; i < todoArticles.length; i++) {
+    toDoItemSection.innerHTML += 
+    `<article class="toDoItemClass" id="toDoItem">
+    <div class="datePartInTODOItem">
+      <span class="toDaysDateClass" id="toDaysDate">${todoArticles[i].toDaysDate}</span>
+      <span class="deadLineDateClass" id="deadLineDate">${todoArticles[i].deadLineDate}</span>
+    </div>
+    <h2 class="toDoNameClass" id="toDoName">${todoArticles[i].toDoName}</h2>
+    <p class="descriptionClass" id="description">${todoArticles[i].description}</p>
+    <p class="fiveDaysToDeadlineP" id="fiveDaysToDeadline"></p>
+    <div class="todoButtonsDiv">
+      <button class="deliteButtonClass" id="deliteButton">Radera</button>
+      <button class="doneButtonClass" id="doneButton">Markera som klar</button>
+    </div>      
+  </article>`;
   }
 }
 
-// Printa kortleken
-let cardString = '';
-myShuffledCardDeck.forEach((card) => {
-  cardString += `
-    <button class="card">
-      <span class="front">♠</span>
-      <span class="back">${card}</span>
-    </button>`;
-});
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = cardString;
-
-document.querySelectorAll('.card').forEach((card) => {
-  card.addEventListener('click', flipCard);
-});
-*/
+updateTodoList();
